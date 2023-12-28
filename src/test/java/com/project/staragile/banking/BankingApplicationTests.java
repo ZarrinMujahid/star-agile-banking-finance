a782bf88602bb9d8e0f3b89ca9083e5457bcb1b9
ZarrinMujahid <144510350+ZarrinMujahid@users.noreply.github.com>
2023-12-28 15:21:47 +0530

    Update BankingApplicationTests.java

--- src/test/java/com/project/staragile/banking/BankingApplicationTests.java
+++ src/test/java/com/project/staragile/banking/BankingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.staragile.banking;
+package com.project.staragile.banking-finance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
